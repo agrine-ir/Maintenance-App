4b1425a58b37279e87bd14358f4c8273a6aab228
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-22 14:22:43 +0330

    Add project files.

---
+++ SetUp-CMD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetUp_CMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
