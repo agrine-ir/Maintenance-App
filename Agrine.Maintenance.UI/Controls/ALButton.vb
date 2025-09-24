2293bb74468367cb45b808504ef981128f9d85b8
Amin Mirzaei <aminmirzaei.og@gmail.com>
2025-09-24 22:00:35 +0330

    - Update ALButton Base Code and Add Characters Folder To Library

--- Agrine.Maintenance.UI/Controls/ALButton.vb
+++ Agrine.Maintenance.UI/Controls/ALButton.vb
@@ -1,42 +1,142 @@
 ﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Runtime.Serialization.Formatters
 Imports System.Windows.Forms
 
-' کلاس دکمه سفارشی
+''' <summary>
+''' Custom button with rounded corners, border size, and border color options.
+''' </summary>
 Public Class ALButton
     Inherits Button
 
+    ' ================================
+    ' Properties
+    ' ================================
+
+    Private _borderRadius As Integer = 20
+    ''' <summary>
+    ''' Gets or sets the corner radius of the button (in pixels).
+    ''' </summary>
+    Public Property BorderRadius As Integer
+        Get
+            Return _borderRadius
+        End Get
+        Set(value As Integer)
+            _borderRadius = value
+            Me.Invalidate() ' Redraw the control
+        End Set
+    End Property
+
+    Private _borderSize As Integer = 2
+    ''' <summary>
+    ''' Gets or sets the thickness of the button border.
+    ''' </summary>
+    Public Property BorderSize As Integer
+        Get
+            Return _borderSize
+        End Get
+        Set(value As Integer)
+            _borderSize = value
+            Me.Invalidate()
+        End Set
+    End Property
+
+    Private _borderColor As Color = Color.Black
+    ''' <summary>
+    ''' Gets or sets the color of the button border.
+    ''' </summary>
+    Public Property BorderColor As Color
+        Get
+            Return _borderColor
+        End Get
+        Set(value As Color)
+            _borderColor = value
+            Me.Invalidate()
+        End Set
+    End Property
+
+    ' ================================
+    ' Constructor
+    ' ================================
+    ''' <summary>
+    ''' Initializes a new instance of the <see cref="CustomButton"/> class with default settings.
+    ''' </summary>
     Public Sub New()
-        ' تنظیمات پیش‌فرض دکمه
+        MyBase.New()
         Me.FlatStyle = FlatStyle.Flat
+        Me.FlatAppearance.BorderSize = 0
+        Me.Size = New Size(120, 45)
         Me.BackColor = Color.MediumSlateBlue
         Me.ForeColor = Color.White
-        Me.Font = New Font("Tahoma", 12, FontStyle.Bold)
-        Me.Size = New Size(120, 50)
-        Me.FlatAppearance.BorderSize = 0
-        Me.Cursor = Cursors.Hand
-        Me.Text = "دکمه من"
+        Me.Text = "دکمه"
     End Sub
 
-    ' تغییر ظاهر هنگام هاور شدن
-    Protected Overrides Sub OnMouseEnter(e As EventArgs)
-        MyBase.OnMouseEnter(e)
-        Me.BackColor = Color.DarkSlateBlue
+    ' ================================
+    ' Methods
+    ' ================================
+
+    ''' <summary>
+    ''' Creates a GraphicsPath with rounded corners based on the given rectangle and radius.
+    ''' </summary>
+    ''' <param name="rect">The rectangle bounds of the button.</param>
+    ''' <param name="radius">The corner radius.</param>
+    ''' <returns>A <see cref="GraphicsPath"/> representing the rounded rectangle.</returns>
+    Private Function GetFigurePath(rect As Rectangle, radius As Integer) As GraphicsPath
+        Dim path As New GraphicsPath()
+        Dim curveSize As Single = radius * 2.0F
+
+        path.StartFigure()
+        path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90)
+        path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90)
+        path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90)
+        path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90)
+        path.CloseFigure()
+
+        Return path
+    End Function
+
+    ''' <summary>
+    ''' Overrides the Paint event to draw a custom button with rounded corners and border.
+    ''' </summary>
+    ''' <param name="pevent">The paint event arguments.</param>
+    Protected Overrides Sub OnPaint(pevent As PaintEventArgs)
+        MyBase.OnPaint(pevent)
+
+        Dim rectSurface As Rectangle = Me.ClientRectangle
+        Dim rectBorder As Rectangle = Rectangle.Inflate(rectSurface, -_borderSize, -_borderSize)
+
+        Dim smoothSize As Integer = If(_borderSize > 0, _borderSize, 2)
+
+        pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias
+
+        ' If rounded corners are enabled
+        If _borderRadius > 2 Then
+            Using pathSurface As GraphicsPath = GetFigurePath(rectSurface, _borderRadius)
+                Using pathBorder As GraphicsPath = GetFigurePath(rectBorder, _borderRadius - _borderSize)
+                    Using penSurface As New Pen(Me.Parent.BackColor, smoothSize)
+                        Using penBorder As New Pen(_borderColor, _borderSize)
+                            ' Draw surface
+                            Me.Region = New Region(pathSurface)
+                            pevent.Graphics.DrawPath(penSurface, pathSurface)
+
+                            ' Draw border
+                            If _borderSize >= 1 Then
+                                pevent.Graphics.DrawPath(penBorder, pathBorder)
+                            End If
+                        End Using
+                    End Using
+                End Using
+            End Using
+        Else
+            ' Normal rectangular button
+            Me.Region = New Region(rectSurface)
+            If _borderSize >= 1 Then
+                Using penBorder As New Pen(_borderColor, _borderSize)
+                    penBorder.Alignment = PenAlignment.Inset
+                    pevent.Graphics.DrawRectangle(penBorder, 0, 0, rectSurface.Width - 1, rectSurface.Height - 1)
+                End Using
+            End If
+        End If
     End Sub
 
-    ' بازگرداندن ظاهر هنگام خروج موس
-    Protected Overrides Sub OnMouseLeave(e As EventArgs)
-        MyBase.OnMouseLeave(e)
-        Me.BackColor = Color.MediumSlateBlue
-    End Sub
-
-    ' تغییر ظاهر هنگام کلیک
-    Protected Overrides Sub OnMouseDown(mevent As MouseEventArgs)
-        MyBase.OnMouseDown(mevent)
-        Me.BackColor = Color.Indigo
-    End Sub
-
-    Protected Overrides Sub OnMouseUp(mevent As MouseEventArgs)
-        MyBase.OnMouseUp(mevent)
-        Me.BackColor = Color.DarkSlateBlue
-    End Sub
 End Class
---
+++ TestApp/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.alButton1 = new Agrine.Maintenance.UI.ALButton();
+            this.SuspendLayout();
+            // 
+            // alButton1
+            // 
+            this.alButton1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.alButton1.BorderColor = System.Drawing.Color.White;
+            this.alButton1.BorderRadius = 10;
+            this.alButton1.BorderSize = 4;
+            this.alButton1.FlatAppearance.BorderSize = 0;
+            this.alButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.alButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.alButton1.ForeColor = System.Drawing.Color.White;
+            this.alButton1.Location = new System.Drawing.Point(249, 111);
+            this.alButton1.Name = "alButton1";
+            this.alButton1.Size = new System.Drawing.Size(234, 112);
+            this.alButton1.TabIndex = 0;
+            this.alButton1.Text = "سلام و درود";
+            this.alButton1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(862, 500);
+            this.Controls.Add(this.alButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Agrine.Maintenance.UI.ALButton alButton1;
+    }
+}
+
